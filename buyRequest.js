e89434e956310421edc3dbf8305b76aea3cf13cd
Shattik <1905008@ugrad.cse.buet.ac.bd>
2024-02-08 20:26:24 +0600

    requestAddedInfo

--- buyRequest.js
+++ buyRequest.js
@@ -55,6 +55,62 @@ router.route("/sme").post(async (req, res) => {
 
 });
 
+router.route("/farmer/info").post(async (req, res) => {
+    const { farmer_nid } = req.body;
+
+    try {
+        let rem = await supabase.any(`select "ongoingLoan" from "Farmer" where "id" = (select "id" from "User" where "nid" = $1)`, [farmer_nid]);
+        if(rem.length == 0) {
+            let response = {
+                remaining: 0,
+            }
+            res.status(200).send(response);
+            return;
+        }
+        let info = await supabase.any(`select "remainingAmount" from "FarmerLoan" where "id" = $1`, [rem[0].ongoingLoan]);
+        let response = {
+            remaining: info[0].remainingAmount,
+        }
+        res.status(200).send(response);
+    }
+    catch (error) {
+        console.error("Error in /buy_request/farmer/info:", error);
+        let respone = {
+            status: "failed",
+            message: "Internal server error",
+        };
+        res.status(500).send(respone);
+    }
+});
+
+router.route("/sme/info").post(async (req, res) => {
+    const { sme_nid } = req.body;
+
+    try {
+        let rem = await supabase.any(`select "ongoingLoan" from "Sme" where "id" = (select "id" from "User" where "nid" = $1)`, [sme_nid]);
+        if(rem.length == 0) {
+            let response = {
+                remaining: 0,
+            }
+            res.status(200).send(response);
+            return;
+        }
+        let info = await supabase.any(`select "remainingAmount" from "SmeLoan" where "id" = $1`, [rem[0].ongoingLoan]);
+        let response = {
+            remaining: info[0].remainingAmount,
+        }
+        res.status(200).send(response);
+    }
+    catch (error) {
+        console.error("Error in /buy_request/sme/info:", error);
+        let respone = {
+            status: "failed",
+            message: "Internal server error",
+        };
+        res.status(500).send(respone);
+    }
+});
+
 router.route("/farmer/submit").post(async (req, res) => {
     const { buyReq, buyItems } = req.body;
     try{
