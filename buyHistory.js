2287def0986c394bf78ea9a2a7afb59143947fac
tahmid-404-20 <1905002@ugrad.cse.buet.ac.bd>
2024-02-08 17:20:20 +0600

    variable error fixed

--- buyHistory.js
+++ buyHistory.js
@@ -1,16 +1,51 @@
-const supabase = require("./db.js");
 const router = require("express").Router();
-const supabase = require('./db.js');
+const supabase = require("./db.js");
 const axios = require("axios");
 
-router.post("/", async (req, res) => {
+router.post("/farmer", async (req, res) => {
   console.log("Hola");
   const productMsUrl = await getProductMsUrl();
   const inventoryUrl = productMsUrl + "/inventory";
 
   console.log(inventoryUrl);
 
-  const req_data = { user_id: req.body.id};
+  const req_data = { user_id: req.body.id };
+
+  try {
+    const response = await axios.post(inventoryUrl, req_data);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/sme", async (req, res) => {
+  console.log("Hola");
+  const productMsUrl = await getProductMsUrl();
+  const inventoryUrl = productMsUrl + "/inventory";
+
+  console.log(inventoryUrl);
+
+  const req_data = { user_id: req.body.id };
+
+  try {
+    const response = await axios.post(inventoryUrl, req_data);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/agent", async (req, res) => {
+  console.log("Hola");
+  const productMsUrl = await getProductMsUrl();
+  const inventoryUrl = productMsUrl + "/inventory";
+
+  console.log(inventoryUrl);
+
+  const req_data = { user_id: req.body.id };
 
   try {
     const response = await axios.post(inventoryUrl, req_data);
